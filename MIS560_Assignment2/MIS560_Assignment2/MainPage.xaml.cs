8ab20000b0d4b4d4f99b5e9c5f7f912acfe89841
rrojo146 <rrojo146@robertmorris.edu>
2014-05-26 12:16:52 -0500

    assignment 2
    Ver1

---
+++ MIS560_Assignment2/MIS560_Assignment2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+
+namespace MIS560_Assignment2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void newNoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //IsolatedStorageSettings.ApplicationSettings["uName"] = txtBox1.Text;
+            NavigationService.Navigate(new Uri("/Page2.xaml", UriKind.Relative));
+        }
+
+        private void MainListNote_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MIS560_Assignment2/MIS560_Assignment2/Page2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+
+namespace MIS560_Assignment2
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void txbNoteEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.txbNoteEntry.Text != "")
+            {
+                IsolatedStorageSettings.ApplicationSettings["lstNoteList"] = (this.txbNoteEntry.Text);
+            }
+        }
+    }
+}
